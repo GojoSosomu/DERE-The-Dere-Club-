856e5c284384ae52f7580acd4af0133a66cb7dbe
GojoSosomu <chemueljhondelapena.04@gmail.com>
2026-07-28 16:55:28 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+let slotsPosition = [
+    { id: "far-left", left: "0%" },
+    { id: "left", left: "20%" },
+    { id: "center", left: "40%" },
+    { id: "right", left: "60%" },
+    { id: "far-right", left: "80%" }
+];
