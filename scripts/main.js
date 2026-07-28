b6380d6eec5dc1b6e1f14130055bd8f9c0622617
GojoSosomu <chemueljhondelapena.04@gmail.com>
2026-07-28 18:50:39 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,26 @@
-let slotsPosition = [
-    { id: "far-left", left: "0%" },
-    { id: "left", left: "20%" },
-    { id: "center", left: "40%" },
-    { id: "right", left: "60%" },
-    { id: "far-right", left: "80%" }
+let activeStage = [
+    { slotId: "far-left", character: null },
+    { slotId: "left",     character: "Kyle" },
+    { slotId: "center",   character: null },
+    { slotId: "right",    character: "Monika" },
+    { slotId: "far-right",character: null }
 ];
+
+
+function spawnCharacter(characterImageSrc, slotId) {
+    let targetSlot = document.getElementById(slotId);
+    targetSlot.innerHTML = "";
+    
+    let characterImg = document.createElement("img");
+    characterImg.src = characterImageSrc;
+    characterImg.className = "character-sprite";
+    
+    targetSlot.appendChild(characterImg);
+}
+
+spawnCharacter("images/Angel Naval Profile.png", "far-right");
+spawnCharacter("images/Angel Naval Profile.png", "right");
+spawnCharacter("images/Angel Naval Profile.png", "center");
+spawnCharacter("images/Angel Naval Profile.png", "left");
+spawnCharacter("images/Angel Naval Profile.png", "far-left");
+
