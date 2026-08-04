0c476bdd00d4eba24af4efd48996e4e30973b14e
GojoSosomu <chemueljhondelapena.04@gmail.com>
2026-08-04 22:50:03 +0800

    Tried to add the parser but failed

--- scripts/main.js
+++ scripts/main.js
@@ -47,7 +47,7 @@ const EventBus = {
 };
 
 const Engine = {
-    initialize() {
+    async initialize() {
         DOM.initialize();
         Screen.initialize();
         Settings.initialize();
@@ -56,6 +56,7 @@ const Engine = {
         Choice.initialize();
         Events.initialize();
         Audio.initialize();
+        await StoryLoader.initialize("../data/scene/year1.json");
     }
 };
 
@@ -74,8 +75,6 @@ const DOM = {
 
         this.startButton = get("#start");
         this.settingButton = get("#setting");
-        this.volumeButton = get("#volume");
-        this.textButton = get("#text");
         this.aboutButton = get("#about");
         this.backButton = get("#back-button"); // this is the button on settings/about scene
         this.backButtonAbout = get("#back-button-ab");
@@ -95,6 +94,10 @@ const DOM = {
 
         this.settingCategory = get(".setting-category");
         this.settingContent = get(".setting-content");
+        this.settingOptionButtons = {
+            volumeButton: get("#volume"),
+            textButton: get("#text")
+        };
 
         this.popups = {
             choice: {
@@ -136,12 +139,14 @@ const Events = {
         on(DOM.aboutButton, "click", () => Screen.open(DOM.pages.about));
         on(DOM.backButtonAbout, "click", () => Screen.back());
         window.onresize = () => {
-            Screen.getOrientation();
+            Screen.updateOrientation();
         }
     }
 };
 
 const Screen = {
+    orientation: null,
+
     initialize() {
         DOM.currentPage = DOM.pages.mainMenu;
 
@@ -152,25 +157,36 @@ const Screen = {
         });
     },
 
-    getOrientation() {
-        let orient = window.innerWidth > window.innerHeight ? "Landscape" : "Portrait";
-        if (orient == "Portrait") {
-            this.hideAllPages();
-            this.show(DOM.pages.orientationInfo);
+    updateOrientation() {
+        const orientation =
+            window.innerWidth > window.innerHeight
+                ? "landscape"
+                : "portrait";
+
+        if (orientation === this.orientation) return;
+
+        this.orientation = orientation;
+
+        if (orientation === "landscape") {
+            this.open(DOM.currentPage);
         } else {
-            this.hideAllPages();
-            this.show(DOM.currentPage);
+            this.hide(DOM.currentPage);
+            this.show(DOM.pages.orientationInfo);
         }
     },
 
     open(page) {
+        if (!page) return;
+
         this.hideAllPages();
-        this.hide(DOM.popups.menu.popup);
+        this.hideAllPopups();
 
-        DOM.previousPage = DOM.currentPage;
-        DOM.currentPage = page;
+        [DOM.previousPage, DOM.currentPage] = [
+            DOM.currentPage,
+            page
+        ];
 
-        this.show(page);
+        this.show(DOM.currentPage);
     },
 
     back() {
@@ -206,22 +222,227 @@ const Screen = {
             : this.hide(element);
     },
 
+    hideAllPopups() {
+        Object.values(DOM.popups).forEach(popup => {
+            this.hide(popup.popup);
+        });
+    },
+
     hideAllPages() {
         Object.values(DOM.pages).forEach(page => this.hide(page));
     }
 };
+
+const StoryLoader = {
+    async initialize(storySrc) {
+        const storyData = await JsonLoader.load(storySrc);
+
+        StoryBuilder.initialize(storyData);
+
+        console.log(GraphBuilder.sceneMap);
+    }
+};
+
+const JsonLoader = {
+    async load(src) {
+        const response = await fetch(src);
+
+        if(!response.ok)
+            throw new Error(`Failed to load '${src}'.`);
+
+        return await response.json();
+    }
+};
+
+const StoryParser = {
+    parse(data) {
+        switch(data.type) {
+            case "speaker":
+                return new SpeakerNode(data);
+            case "choice":
+                return new ChoiceNode(data);
+            case "goto":
+                return new GotoNode(data.scene);
+        }
+    }
+}
+
+const GraphBuilder = {
+    initialize() {
+        this.sceneMap = new Map();
+
+        this.pendingGoto = [];
+
+        this.currentScene = null;
+
+        this.previousNode = null;
+    },
+    
+    beginScene(sceneNode) {
+        this.currentScene = sceneNode;
+
+        this.sceneMap.set(sceneNode.id, sceneNode);
+
+        this.previousNode = null;
+    },
+
+    connectNodes(node) {
+        if(this.previousNode == null)
+            this.currentScene.firstNode = node;
+        else 
+            this.previousNode.next = node;
+
+        if(node.type == "choice")
+            return;
+
+        this.previousNode = node;
+
+        if(node instanceof GotoNode)
+            this.pendingGoto.push(node);
+    },
+
+    finalize() {
+        for(const gotoNode of this.pendingGoto) {
+            gotoNode.next = this.sceneMap.get(gotoNode.sceneId).firstNode;
+            if(gotoNode.next == null)
+                throw new Error(`Scene '${gotoNode.sceneId}' does not exist.`);
+        }
+    }
+}
+
+class Node {
+    constructor(type) {
+        this.type = type;
+    }
+}
+
+class Scene {
+    constructor(id, background) {
+        this.id = id;
+        this.background = background;
+
+        this.firstNode = null;
+        this.lastNode = null;
+    }
+}
+
+class LinearNode extends Node {
+    constructor(type) {
+        super(type);
+
+        this.next = null;
+    }
+}
+
+class SpeakerNode extends LinearNode {
+    constructor(data) {
+        super("speaker");
+
+        this.speaker = data.speaker;
+        this.text = data.text;
+        this.position = data.position ?? null;
+    }
+}
+
+class ChoiceNode extends Node {
+    constructor(data) {
+        super("choice");
+
+        this.choices = [];
+    }
+}
+
+class ChoiceOption {
+    constructor(text) {
+        this.text = text;
+
+        this.next = null;
+    }
+}
+
+class CommandNode extends LinearNode {
+    constructor(type) {
+        super(type);
+    }
+}
+
+class GotoNode extends CommandNode {
+    constructor(sceneId) {
+        super("goto");
+
+        this.sceneId = sceneId;
+    }
+}
+
+const StoryBuilder = {
+    initialize(storyData) {
+        GraphBuilder.initialize();
+
+        for(const [sceneId, sceneData] of Object.entries(storyData.scenes)) {
+            const scene = new Scene(
+                sceneId,
+                sceneData.background
+            );
+
+            GraphBuilder.beginScene(scene);
+
+            this.buildDialogue(sceneId, sceneData.dialogue);
+        }
+
+        GraphBuilder.finalize();
+    },
+
+    buildDialogue(sceneId, dialogue) {
+        let firstNode = null;
+
+        for(const data of dialogue) {
+            const previous = GraphBuilder.previousNode;
+
+            GraphBuilder.previousNode = null;
+            const node = StoryParser.parse(data);
+
+            if(firstNode == null)
+                firstNode = node;
+
+            
+
+            if(data.type === "choice") {
+                GraphBuilder.connectNodes(node);
+                for(const choice of data.choices)
+                    GraphBuilder.previousNode = this.buildDialogue(sceneId, choice.dialogue);
+                    node.choices.push(
+                        GraphBuilder.previousNode
+                    );
+
+                    GraphBuilder.previousNode = previous;
+            } else
+                GraphBuilder.connectNodes(node);
+        }
+
+        return firstNode;
+    }
+}
+
+const StoryRunner = {
+    initialize(storyGraph) {
+
+    }
+}
 
 const Settings = {
     data: {
         masterVolume: 100,
         sfxVolume: 100,
         bgVolume: 100,
-        textSpeed: 50
+        textSpeed: 50,
+        tab: "volume"
     },
 
     initialize() {
-        on(DOM.volumeButton, "click", () => this.changeContent(DOM.volumeButton));
-        on(DOM.textButton, "click", () => this.changeContent(DOM.textButton));
+        this.refresh();
+
+        on(DOM.settingOptionButtons.volumeButton, "click", () => this.changeContent(DOM.settingOptionButtons.volumeButton));
+        on(DOM.settingOptionButtons.textButton, "click", () => this.changeContent(DOM.settingOptionButtons.textButton));
         on(DOM.backButton, "click", () => Screen.back());
 
         EventBus.on("setting:change", data => Object.assign(this.data, data));
@@ -287,6 +508,10 @@ const Settings = {
         if (this.currentContent === "text") {
             DOM.settingContent.innerHTML = this.openTextSettings();
         }
+    },
+
+    refresh() {
+        this.changeContent(DOM.settingOptionButtons[this.data.tab + "Button"]);
     }
 };
 
