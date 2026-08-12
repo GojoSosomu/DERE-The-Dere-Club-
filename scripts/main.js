2f830511ab8dd8f35da771d7dfb9b7e4da116d3a
GojoSosomu <chemueljhondelapena.04@example.com>
2026-08-12 08:31:33 +0800

    fixed minor issues

--- scripts/main.js
+++ scripts/main.js
@@ -172,6 +172,8 @@ const Events = {
 
             Screen.open(DOM.pages.loading);
 
+            await Sleep(5000);
+
             if (await SaveLoad.continueGame())
                 return;
 
@@ -576,7 +578,7 @@ const StoryLoader = {
         if (StoryGraph.storyMap.has(yearSelection))
             return;
 
-        const storyData = await JsonLoader.load(`data/scene/${yearSelection}.json`);
+        const storyData = await JsonLoader.load(`data/year/${yearSelection}.json`);
 
         StoryBuilder.initialize(storyData);
 
